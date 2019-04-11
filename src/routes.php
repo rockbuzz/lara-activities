b9d07512c7a536a22373633ba6387cae59142eee
Tiago Lopes <tiago.desenvolvedorweb@gmail.com>
2019-04-11 11:02:45 -0300

    feature/controllers_and_views mais ajustes gerais

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get(config('activities.routes.index.uri'),[
+Route::get(config('activities.routes.index.uri'), [
     'middleware' => config('activities.routes.index.middleware'),
     'as' => config('activities.routes.index.as'),
     'uses' => config('activities.routes.index.uses')
