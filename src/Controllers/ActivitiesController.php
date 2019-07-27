8708f8d433011f3c4ede9a7b2a1e0513ed754ef7
Tiago Lopes <tiago.desenvolvedorweb@gmail.com>
2019-07-27 10:40:35 -0300

    ordem de apresentacao

--- src/Controllers/ActivitiesController.php
+++ src/Controllers/ActivitiesController.php
@@ -9,10 +9,12 @@ class ActivitiesController extends Controller
 {
     public function index()
     {
-        $activities = Activity::paginate(30);
+        $activities = Activity::latest()->paginate(30);
 
         if ($type = request('type')) {
-            $activities = Activity::where('type', 'like', "%{$type}%")->paginate(30);
+            $activities = Activity::where('type', 'like', "%{$type}%")
+                ->latest()
+                ->paginate(30);
         }
 
         return view('activities::index', compact('activities'));
